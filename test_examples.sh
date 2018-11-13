8871f164b263df929f8eef37b3c0992e4acd00fd
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2018-11-13 13:39:25 +0700

    add examples tests

--- test_examples.sh
+++ test_examples.sh
@@ -7,6 +7,8 @@ dub fetch dtools
 # extract examples
 dub run dtools:tests_extractor -- -i source -o out
 
+git clone https://github.com/libmir/mir-core
+
 # compile the examples
 for file in $(find out -name "*.d") ; do
     echo "Testing: $file"
